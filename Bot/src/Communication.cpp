bb3e16e3b7ffc7ea12a936ce35d58872aeee09b6
Daniel Bartov <45463423+danbar2003@users.noreply.github.com>
2020-10-29 14:41:51 +0200

    modified Communjication constractor

--- Bot/src/Communication.cpp
+++ Bot/src/Communication.cpp
@@ -1,13 +1,12 @@
 #include "Communication.h"
+#include "network.h"
 
 Communication::Communication()
 {
 
 	udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if (udp_sock == INVALID_SOCKET)
-	{
-		exit(1);
-	}
+	check(udp_sock, "can't create udp_sock");
+	
 	// Create a server hint structure for the server
 	sockaddr_in serverHint;
 	serverHint.sin_addr.S_un.S_addr = ADDR_ANY; // Us any IP address available on the machine
@@ -15,29 +14,17 @@ Communication::Communication()
 	serverHint.sin_port = htons(54000); // Convert from little to big endian
 
 	// Try and bind the socket to the IP and port
-	if (bind(udp_sock, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
-	{
-		printf("%s %d\n", "can't bind socket", WSAGetLastError());
-		exit(1);
-	}
+	check(bind(udp_sock, (sockaddr*)&serverHint, sizeof(serverHint)), "can't bind udp socket");
 
 	tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (tcp_sock == INVALID_SOCKET)
-	{
-		printf("%s %d\n", "Error creating tcp_sock", WSAGetLastError());
-		exit(1);
-	}
+	check(tcp_sock, "can't create tcp_sock");
 
 	serverHint.sin_addr.S_un.S_addr = ADDR_ANY; // Us any IP address available on the machine
 	serverHint.sin_family = AF_INET; // Address format is IPv4
 	serverHint.sin_port = htons(55000); // Convert from little to big endian
 
-	if (bind(tcp_sock, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
-	{
-		printf("%s %d\n", "can't bind socket", WSAGetLastError());
-		exit(1);
-	}
-
+	check(bind(tcp_sock, (sockaddr*)&serverHint, sizeof(serverHint)), "can't bind tcp socket");
+	
 	//tcp_sock = tcp;
 	NetTree = new NetworkTree(udp_sock, tcp_sock);
 
