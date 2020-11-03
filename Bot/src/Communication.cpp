ad55a151597a7bbcadcdbffbf590ecb0526eb9fa
Daniel Bartov <45463423+danbar2003@users.noreply.github.com>
2020-11-03 21:56:47 +0200

    Revert "ignoring host packets"
    This reverts commit 99b9f51fe165d9b636df272ec300d7410726c6a9.

--- Bot/src/Communication.cpp
+++ Bot/src/Communication.cpp
@@ -125,15 +125,8 @@ void Communication::HandleIncomingsUDP()
 
 		recvfrom(udp_sock, buf, BOTNET_PACK_SIZE, 0, (struct sockaddr*)&client, &client_size);
 		
-		//if the recv came from this machine
-		if ((bool*)&[=]()
-			{
-				for (size_t i = 0; DEC_ADAPTER_IPS_ARR[i].hip; i++)				
-					if (DEC_ADAPTER_IPS_ARR[i].hip == client.sin_addr.s_addr)
-						return 1;
-			})
-			continue;
 		
+
 		switch (p->type)
 		{
 		case SYNC_REPLY:
