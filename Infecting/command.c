82587ff1f0e606d7559852653c3851fd473de876
Daniel Bartov <45463423+danbar2003@users.noreply.github.com>
2020-12-04 13:56:49 +0200

    scaning the network stage 2
    capturing the arp replys.
    
    stage 3 - send the results.

--- Infecting/command.c
+++ Infecting/command.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #define HAVE_REMOTE
 #define WPCAP
@@ -19,6 +20,12 @@
 
 typedef struct
 {
+	pcap_t* fp;
+	PIP_ADAPTER_INFO adapter;
+}send_params;
+
+typedef struct
+{
 	BYTE b1;
 	BYTE b2;
 	BYTE b3;
@@ -26,6 +33,10 @@ typedef struct
 	BYTE b5;
 	BYTE b6;
 } MAC_ADDR;
+
+//globals
+static HANDLE act_threads[ACTS_NUM];
+static send_params parameters;
 
 static PIP_ADAPTER_INFO correspoding_adapter(pcap_if_t* pcap_adapter, PIP_ADAPTER_INFO pAdapters)
 {
@@ -51,18 +62,19 @@ static PIP_ADAPTER_INFO correspoding_adapter(pcap_if_t* pcap_adapter, PIP_ADAPTE
 	return NULL;
 }
 
-static HANDLE act_threads[ACTS_NUM];
-
-static void build_packet(u_char* packet, PIP_ADAPTER_INFO adapter)
+static DWORD WINAPI send_packets(LPVOID lparam)
 {
+	PIP_ADDR_STRING addr_lst;
 	ether_hdr* ether_header;
 	arp_ether_ipv4* arp_header;
+	u_char packet[sizeof(ether_hdr) + sizeof(arp_ether_ipv4)];
 	
+	addr_lst = &parameters.adapter->IpAddressList;
 	ether_header = (ether_hdr*)packet;
 	arp_header = (arp_ether_ipv4*)(packet + sizeof(ether_hdr));
-	
+
 	//ETHERNET LAYER
-	memcpy(ether_header->src_addr, adapter->Address, ETH_ALEN); //this adapter's mac
+	memcpy(ether_header->src_addr, parameters.adapter->Address, ETH_ALEN); //this adapter's mac
 	memset(ether_header->dest_addr, 0xff, ETH_ALEN); //broadcast
 	ether_header->frame_type = htons(ETH_P_ARP); //LAYER3:ARP (0x0806)
 
@@ -73,8 +85,32 @@ static void build_packet(u_char* packet, PIP_ADAPTER_INFO adapter)
 	arp_header->plen = 4;
 	arp_header->op = htons(1); // who has
 	memcpy(arp_header->sha, ether_header->src_addr, ETH_ALEN);
-	inet_pton(AF_INET, adapter->IpAddressList.IpAddress.String, &arp_header->spa);
 	memset(arp_header->tha, 0, ETH_ALEN);
+
+	u_long target;
+	u_long netmask;
+
+	while (addr_lst)
+	{
+		inet_pton(AF_INET, addr_lst->IpMask.String, &netmask);
+		inet_pton(AF_INET, addr_lst->IpAddress.String, &target);
+
+		netmask = htonl(netmask);
+		target = htonl(target);
+		target &= netmask;
+
+		inet_pton(AF_INET, addr_lst->IpAddress.String, &arp_header->spa);
+		while (netmask != 0xffffffff)
+		{
+			arp_header->tpa = htonl(target);
+			pcap_sendpacket(parameters.fp, packet, sizeof(packet));
+			target++;
+			netmask++;
+		}
+
+		addr_lst = addr_lst->Next;
+	}
+	return 0;
 }
 
 DWORD WINAPI scan(LPVOID lparam)
@@ -96,15 +132,14 @@ DWORD WINAPI scan(LPVOID lparam)
 	struct bpf_program fcode;
 	int res;
 	struct pcap_pkthdr* pkt_header;
-	const u_char* pkt_data;
+	const u_char* pkt_data = NULL;
 	ULONG32 netmask;
-
 
 	pcap_findalldevs(&alldevs, NULL);
 	
 	for (temp = alldevs; temp; temp = temp->next)
 	{
-		//open adapter
+		/*open the adapter*/
 		fp = pcap_open(
 			temp->name,
 			sizeof(ether_hdr) + sizeof(arp_ether_ipv4),
@@ -139,18 +174,46 @@ DWORD WINAPI scan(LPVOID lparam)
 			return -1;
 		}
 
+		/*update globals for send_packet*/
+		parameters.adapter = correspoding_adapter(temp, pAdapterInfo);
+		parameters.fp = fp;
+
+		/*start send_packet thread*/
+		HANDLE hThread;
+		hThread = CreateThread(NULL, 0, send_packets, NULL, 0, NULL);
+		if (!hThread)
+		{
+			pcap_close(fp);
+			continue;
+		}
+
 		ether_hdr* ether_header;
 		arp_ether_ipv4* arp_header;
+
 		/* Retrieve the packets */
+		int counter = 0;
 		while ((res = pcap_next_ex(fp, &pkt_header, &pkt_data)) >= 0) {
 
 			/* Timeout elapsed */
 			if (res == 0)
+			{
+				if (WaitForSingleObject(hThread, 0) == WAIT_OBJECT_0)
+				{
+					CloseHandle(hThread);
+					break;
+				}
 				continue;
+			}
 
 			ether_header = (ether_hdr*)pkt_data;
 			arp_header = (arp_ether_ipv4*)(pkt_data + sizeof(ether_hdr));
-			
+
+			struct in_addr a;
+			if (arp_header->op == htons(2))
+			{
+				a.s_addr = arp_header->spa;
+				printf("%s %s\n", "reply from ip:", inet_ntoa(a));
+			}
 		}
 
 		//close adapter
