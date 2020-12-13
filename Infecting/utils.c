39f003471fb100cfe9e4f6a799c83356419c1f01
Daniel Bartov <45463423+danbar2003@users.noreply.github.com>
2020-12-13 23:27:44 +0200

    generic packet crafting
    added a function for a generic arp packets crafting

--- Infecting/utils.c
+++ Infecting/utils.c
@@ -1,6 +1,41 @@
 #include "utils.h"
 
-PIP_ADAPTER_INFO corresponding_adapter(pcap_if_t* pcap_adapter, PIP_ADAPTER_INFO pAdapters)
+void build_packet(
+	u_char* packet,
+	u_long src_ip,
+	u_long dst_ip,
+	BYTE src_mac[ETH_ALEN],
+	BYTE dst_mac[ETH_ALEN],
+	short opcode
+)
+{
+	ether_hdr* ethernet_header;
+	arp_ether_ipv4* arp_header;
+
+	ethernet_header = (ether_hdr*)packet;
+	arp_header = (arp_ether_ipv4*)(packet + sizeof(ether_hdr));
+
+	/* ethernet layer */
+	memcpy(ethernet_header->src_addr, src_mac, ETH_ALEN);
+	memcpy(ethernet_header->dest_addr, dst_mac, ETH_ALEN);
+	ethernet_header->frame_type = htons(ETH_P_ARP);
+
+	/* network layer */
+	arp_header->htype = htons(ARP_HTYPE_ETHER);
+	arp_header->ptype = htons(ARP_PTYPE_IPv4);
+	arp_header->hlen = 6;
+	arp_header->plen = 4;
+	arp_header->op = htons(opcode); // who has
+	memcpy(arp_header->sha, src_mac, ETH_ALEN);
+	arp_header->spa = src_ip;
+	memcpy(arp_header->tha, dst_mac, ETH_ALEN);
+	arp_header->tpa = dst_ip;
+}
+
+PIP_ADAPTER_INFO corresponding_adapter(
+	pcap_if_t* pcap_adapter, 
+	PIP_ADAPTER_INFO pAdapters
+)
 {
 	const char* str1 = pcap_adapter->name;
 	const char* str2;
--- Infecting/utils.h
+++ Infecting/utils.h
@@ -3,4 +3,18 @@
 #include <pcap/pcap.h>
 #include <iphlpapi.h>
 
-PIP_ADAPTER_INFO corresponding_adapter(pcap_if_t* pcap_adapter, PIP_ADAPTER_INFO pAdapters);
+#include "network_headers.h"
+
+void build_packet(
+	u_char* packet, 
+	u_long src_ip, 
+	u_long dst_ip, 
+	BYTE src_mac[ETH_ALEN], 
+	BYTE dst_mac[ETH_ALEN],
+	short opcode
+);
+
+PIP_ADAPTER_INFO corresponding_adapter(
+	pcap_if_t* pcap_adapter, 
+	PIP_ADAPTER_INFO pAdapters
+);
